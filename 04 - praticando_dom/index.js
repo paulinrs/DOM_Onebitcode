e36b3ed1a5b263079d9d34222369f49ab7aeaf5b
ruby --version <paulinn.rs@gmail.com>
2023-09-10 10:15:24 -0300

    Novo Usuário

--- 04 - praticando_dom/index.js
+++ 04 - praticando_dom/index.js
@@ -1,3 +1,48 @@
 function addContact() {
+    const contactSection = document.getElementById('contacts-list')
+    
+    const h3 = document.createElement('h3')
+    h3.innerText = "Usuário"
+    
 
+    const ul = document.createElement('ul')
+
+    const nameLi = document.createElement('li')
+    nameLi.innerText = "Nome: "
+    const nameInput = document.createElement('input')
+    nameInput.type = 'text'
+    nameInput.name = 'fullname'
+    nameLi.appendChild(nameInput)
+    ul.appendChild(nameLi)
+    ul.appendChild(document.createElement('br'))
+
+    const phoneLi = document.createElement('li')
+    phoneLi.innerText = "Telefone: "
+    const phoneInput = document.createElement('input')
+    phoneInput.type = 'tel'
+    phoneInput.name = 'phone'
+    phoneLi.appendChild(phoneInput)
+    ul.appendChild(phoneLi)
+    ul.appendChild(document.createElement('br'))
+
+    const passLi = document.createElement('li')
+    passLi.innerText = "Senha: "
+    const passInput = document.createElement('input')
+    passInput.type = 'password'
+    passInput.name = 'pass'
+    passLi.appendChild(passInput)
+    ul.appendChild( passLi)
+    ul.appendChild(document.createElement('br'))
+
+    const addressLi = document.createElement('li')
+    addressLi.innerHTML = '<label for="address">Endereço: </label>'
+    const addressInput = document.createElement('input')
+    addressInput.type = 'text'
+    addressInput.name = 'address'
+    addressInput.id = 'address'
+    addressLi.appendChild(addressInput)
+    ul.appendChild(addressLi)
+    ul.appendChild(document.createElement('br'))
+
+    contactSection.append(h3, ul)
 }
