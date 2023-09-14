5fca55650482153333f1c449d51d07d66855b31c
ruby --version <paulinn.rs@gmail.com>
2023-09-13 21:53:41 -0300

    alerta escalação

--- exercicio1/index.js
+++ exercicio1/index.js
@@ -1,3 +1,9 @@
 function addPlayer() {
-    const position 
+    const position = document.getElementById('position').value
+    const name = document.getElementById('name').value
+    const number = document.getElementById('number').value
+   
+   const confirmation = confirm ("Escalar " + name + " como " + position + "?")
+
+
 }
