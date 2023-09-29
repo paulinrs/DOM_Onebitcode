9c7deacc58957011b4588337d48b70a21bcc95c6
ruby --version <paulinn.rs@gmail.com>
2023-09-29 20:24:15 -0300

    Trabalhando com Eventos no Javascript

--- 06-Trabalhando com eventos no Javascript/index.js
+++ 06-Trabalhando com eventos no Javascript/index.js
@@ -1,13 +1,26 @@
 function register(ev) {
     console.log(ev)
-    const userName = ev.currentTarget.children.userName
-    const password = ev.currentTarget.children.password
-    const passwordConfirmation = ev.currentTarget.children.passwordConfirmation
-
+    const sectionElement = ev.currentTarget.parentNode
+    const username = sectionElement.children.username.value
+    const password = sectionElement.children.password.value
+    const passwordConfirmation = sectionElement.children.passwordConfirmation.value
+  
     if (password === passwordConfirmation) {
-        alert('Usuário')
+      alert("Usuário " + username + " registrado!")
+    } else {
+      alert("As senhas não conferem")
     }
-}
-const button = document.getElementById('register-button')
-
-button.addEventListener('click', register)
+  }
+  
+  function removeEvent() {
+    button.removeEventListener("click", register)
+    alert("Event Removed")
+  }
+  
+  const button = document.getElementById("register-button")
+  
+  button.addEventListener("click", register)
+  
+  button.addEventListener("mouseover", function (ev) {
+    console.log(ev)
+  })
